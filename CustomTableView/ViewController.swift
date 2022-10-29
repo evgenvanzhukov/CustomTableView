c5f078dfaea97c5890878c736e9de2fba5319c2c
evgen z <evgen-2208@ya.ru>
2022-10-29 20:49:49 +0400

    Initial Commit

---
+++ CustomTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTableView
+//
+//  Created by Evgen on 29/10/2022.
+//  Copyright © 2022 Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
